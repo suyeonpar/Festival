8643d038fda6e6b371cba87fe0851b4967191383
박수연 <sksrbdpdy@naver.com>
2023-09-06 13:20:51 +0900

    다울주소변환

--- src/pages/Datepicker.js
+++ src/pages/Datepicker.js
@@ -19,7 +19,7 @@ function Datepicker() {
     useEffect(()=>{
         var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
         var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(35.8683397, 128.593975), //지도의 중심좌표.
+        center: new kakao.maps.LatLng(35.8683397, 128.593975), //지도의 중심좌표. 다울주소전환서에서 좌표 가져오면된다
         level: 3 //지도의 레벨(확대, 축소 정도)
     };
     var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
